c8bb6ce4621c5d28d9abfea9109099ddd575ed13
varsha13-png <ayushpinky07@gmail.com>
2026-03-20 13:03:24 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: '/api',
+  baseURL: process.env.REACT_APP_API_URL ? `${process.env.REACT_APP_API_URL}/api` : 'http://localhost:5000/api',
   headers: { 'Content-Type': 'application/json' }
 });
 
