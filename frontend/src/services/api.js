f16bd62951e30f6ca4aba8b99fcd60a825359c52
varsha13-png <ayushpinky07@gmail.com>
2026-03-20 13:20:04 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL ? `${process.env.REACT_APP_API_URL}/api` : 'http://localhost:5000/api',
+  baseURL: 'https://careconnect-rfal.onrender.com',
   headers: { 'Content-Type': 'application/json' }
 });
 
